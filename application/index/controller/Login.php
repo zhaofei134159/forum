727fbc0e5b9faed676ad1b08a5e246b59c704140
Zhao Fei <zhaofei@51smart.com>
2017-11-07 16:10:55 +0800

    smtp

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -6,7 +6,8 @@ use app\index\model\User;
 
 use think\Session;
 use think\Config;
-use \extend\smtp;
+use smtp;
+
 
 
 class Login extends Common
@@ -106,7 +107,7 @@ class Login extends Common
         $mailtitle = read_conf('website')['web_name'].' , 注册成功';//邮件主题
         //邮件内容
         $mailcontent = "<h2> ".read_conf('website')['web_name']." , 注册成功</h2>";
-        $mailcontent .= "<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;点击 <a href='http://".read_conf('website')['email_callback']."/login/activate_email?addr=".base64_encode($email)."&start=".base64_encode('forum')."&token=".base64_encode($email.'zhaofei')."'>链接</a> 激活邮箱成为博主，就可以发表属于自己的文章。（如果不是本人操作，请忽略本条信息）</h3>";
+        $mailcontent .= "<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;点击 <a href='http://".read_conf('website')['email_callback']."/index/login/activate_email?addr=".base64_encode($email)."&start=".base64_encode('forum')."&token=".base64_encode($email.'zhaofei')."'>链接</a> 激活邮箱，完成注册。（如果不是本人操作，请忽略本条信息）</h3>";
         
         //************************ 配置信息 ****************************
         $smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
