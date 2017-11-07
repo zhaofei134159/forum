8dda5c0d7fddf8359004091f96ba8fab855318be
Zhao Fei <zhaofei@51smart.com>
2017-11-07 17:09:47 +0800

    1

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '172.93.37.18',
     // 数据库名
     'database'        => 'forum',
     // 用户名
