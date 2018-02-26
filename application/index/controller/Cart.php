93abbb1334fc68a12dcff092dc941e8d2c94c209
Zhao Fei <zhaofei@51smart.com>
2018-02-26 16:25:15 +0800

    21321

---
+++ application/index/controller/Cart.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;
+
+use app\index\model\User;
+use app\index\model\Cate;
+use app\index\model\Plate;
+
+use think\Session;
+use think\Config;
+use smtp;
+
+# 帖子
+class Cart extends Common
+{	
+
+	public function __construct(){
+		parent::__construct();	
+	}
+
+	public function index(){
+		
+        return $this->view->fetch('index');
+	}
+}
