636f8aad1b42226364005e04b415f1a5ffab3864
zhaofei <13700530350@163.com>
2018-03-04 22:01:04 +0800

    1231

--- application/admin/controller/Cart.php
+++ application/admin/controller/Cart.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 use app\admin\model\Admin as Admin_user;
 use app\admin\model\User as Home_user;
 use app\admin\model\Cate as Admin_cate;
+use app\admin\model\Plate as Admin_plate;
 use think\Session;
 use think\Config;
 
@@ -21,7 +22,35 @@ class Cart extends Common
 	}
     
     public function index(){
+        $get = input('get.');
+
+        $where = array();
+        if(!empty($get['title'])){
+            $where['title'] = array('like','%'.$get['title'].'%'); 
+        }
+        if($get['plateId']){
+            $where['plateId'] = $get['plateId'];
+        }
+        if($get['is_del']!='-1'){ 
+            $where['is_del'] = $get['is_del']; 
+        }
+
+        $where['cartId'] = 0;
+        $carts = Cart::where($where)->order('ctime','desc')->paginate(10, false);
+        $page = $carts->render();
+
+        $plates = Admin_plate::all(['is_del'=>0]);
+        $plates = objToArray($plates);
+
+        $users = Home_user::all(['is_del'=>0]);
+        $users = objToArray($users);
+
+
         $data = array(
+                'carts'=>$carts,
+                'page'=>$page,
+                'plates'=>$plates,
+                'users'=>$users,
             );
         return $this->view->fetch('index',$data);
     }
