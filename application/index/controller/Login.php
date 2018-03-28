b4de4966e17e4852cd1760e44782b6c2f9e441be
Zhao Fei <zhaofei@51smart.com>
2018-03-28 16:14:28 +0800

    update forum

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -213,7 +213,7 @@ class Login extends Common
         $mailcontent .= "<h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;点击 <a href='http://".read_conf('website')['email_callback']."/index/login/activate_email?addr=".base64_encode($email)."&start=".base64_encode('forum')."&token=".base64_encode($email.'zhaofei')."'>链接</a> 激活邮箱，完成注册。（如果不是本人操作，请忽略本条信息）</h3>";
         
         //************************ 配置信息 ****************************
-        Loader::import('smtp', EXTEND_PATH);
+        Loader::import('smtp', APP_PATH);
         $smtp = new \smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
         $smtp->debug = false;//是否显示发送的调试信息
         $state = @$smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
