3f889411dba21a78e5e6750c8f7732b8e9021795
zhaofei <13700530350@163.com>
2018-05-08 21:54:22 +0800

    forum

--- application/index/controller/Ucenter.php
+++ application/index/controller/Ucenter.php
@@ -593,6 +593,7 @@ class Ucenter extends Common
         
         $security = Security::where($where)->select();
         var_dump($security);
+        var_dump(!empty($security));
         # 问题
         $problem = read_conf('security')['security'];
        
